89ddafa0b52aaf9a8cb5d22149d8e4aa7590c18d
usatyo <yu0424.math8128@gmail.com>
2021-09-20 16:43:22 +0900

    雛形作った Co-authored-by: Nano <Nano3013@users.noreply.github.com>

---
+++ frontend/src/components/button.js
@@ -0,0 +1,7 @@
+import styles from "../styles/button.module.css"
+
+const Footer = () => {
+    return <div className={styles.button}></div>
+};
+
+export default Footer;
--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,7 +1,9 @@
+import styles from "../styles/footer.module.css"
+
 const Footer = () => {
-    return <div>
-        team garigarikun
-    </div>;
+    return <div className={styles.footerwrapper}>
+        <p className={styles.footertitle}>team garigarikun</p>
+    </div>
 };
 
 export default Footer;
