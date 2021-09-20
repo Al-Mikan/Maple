e2f571ed3800f3895b513c21932ba59c7da53e48
USK314 <usk31101021@gmail.com>
2021-09-20 13:57:15 +0900

    データベース作った

---
+++ api/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from .database import Base
+
+
+class Posts(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    garigariname = Column(String, nullable=False)
+    imagePath = Column(String, nullable=False)
+    comment = Column(String, nullable=True)
+    lat = Column(Float, nullable=False)
+    lng = Column(Float, nullable=False)
+    genre = Column(String, nullable=False)
+    favorites = Column(Integer, nullable=False)
+    createdAt = Column(DateTime, nullable=False)
