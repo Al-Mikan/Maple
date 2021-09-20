3473e282d9c50cddf3f1057133fd2bf46f8194e4
Nano3013 <kuma213213@icloud.com>
2021-09-20 11:49:31 +0900

    chakraUI入れた

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from "react"
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react"
 
 function App() {
   return (
+    <ChakraProvider>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +24,7 @@ function App() {
         </a>
       </header>
     </div>
+    </ChakraProvider>
   );
 }
 
