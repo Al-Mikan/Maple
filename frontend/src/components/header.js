99b818fae05128a610e1b84d89a681125b13c226
Nano3013 <kuma213213@icloud.com>
2021-09-20 14:19:01 +0900

    scss

---
+++ frontend/src/components/header.js
@@ -0,0 +1,9 @@
+import styles from "../styles/header.module.scss"
+
+const Header = () => {
+    return <div className={styles.Headerwrapper} >
+        <p>GariGari</p>
+    </div>;
+};
+
+export default Header;
---
+++ frontend/src/pages/Toppage.js
@@ -0,0 +1,9 @@
+import styles from "../styles/Toppage.module.scss"
+
+const Toppage = () => {
+    return <div>
+        aaaaa
+    </div>;
+};
+
+export default Toppage;
