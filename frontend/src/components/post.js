571f7a922658d6740413ad0396a116c8b822fe33
usatyo <yu0424.math8128@gmail.com>
2021-10-03 12:26:09 +0900

    プレビューできた

--- frontend/src/components/post.js
+++ frontend/src/components/post.js
@@ -26,6 +26,7 @@ const Post = () => {
   let [lng, setLng] = useState("");
   const imageRef = useRef();
   const [load, setLoad] = useState(false);
+  const [fileUrl, setFileUrl] = useState(null);
 
   let handleInputChange = (e) => {
     console.log(e);
@@ -50,6 +51,12 @@ const Post = () => {
     await postToServer("おれ", value, lat, lng, imageRef.current);
     setLoad(false);
   };
+
+  const handlePreview = async (event) => {
+    const imageFile = event.target.files[0];
+    const imageUrl = URL.createObjectURL(imageFile);
+    setFileUrl(imageUrl);
+  }
 
   return (
     <div>
@@ -85,7 +92,9 @@ const Post = () => {
               ref={imageRef}
               required
               accept="image/jpeg, image/png"
+              onChange={handlePreview}
             />
+            <img src={fileUrl} alt={fileUrl}/>
           </ModalBody>
           <ModalBody>
             <Text mb="8px">コメント</Text>
